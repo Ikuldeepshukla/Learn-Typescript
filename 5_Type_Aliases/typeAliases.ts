c4ff66d552848479516732576d79d02dc7d52300
Kuldeep <kuldeepshukla09496@gmail.com>
2022-12-30 11:51:15 +0530

    type aliases

---
+++ 5_Type_Aliases/typeAliases.ts
@@ -0,0 +1,12 @@
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
+};
+
+function createUser(user: User): User{
+    return user;
+} 
+
+createUser({name: "", email: "", isActive: false});
+export {};
