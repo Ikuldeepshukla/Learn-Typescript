016955c762a818d3312e80dc7bf597bff8242a71
Kuldeep <kuldeepshukla09496@gmail.com>
2023-01-02 10:52:01 +0530

    learn - optional and readonly type

--- 4_Objects/objects.ts
+++ 4_Objects/objects.ts
@@ -14,3 +14,33 @@ createUser(newUser); // no error - odd behaviour of object in ts
 function createCourse():{name: string, price: number}{
   return {name: "reactjs", price: 399}
 }
+
+type User = {
+  readonly _id: string
+  name: string
+  email: string
+  isActive: boolean
+  creditCardDetails?: number
+}
+
+type cardNumber = {
+  cardNumber: string
+}
+
+type cardDate = {
+  cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+  cvv: number
+}
+
+let myUser: User = {
+  _id: "12345",
+  name: "h",
+  email: "h@h.com",
+  isActive: false,
+} 
+
+myUser.email = "k@k.com";
+// myUser._id = "123"; // error as the value is read only
