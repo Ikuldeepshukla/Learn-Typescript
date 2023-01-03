fbfcc1edf03c122188da5fea2e1c6d041b068877
Kuldeep <kuldeepshukla09496@gmail.com>
2023-01-03 11:03:11 +0530

    learn - tuples

---
+++ 7_Tuples/tuples.js
@@ -0,0 +1,7 @@
+//when we want specific thing in order in an array
+// const user: (string | number | boolean)[] = ["ks", 1, true];
+var User;
+User = ["ks", 123, true]; // order is maintained
+var rgb = [255, 123, 112];
+var User2;
+User2 = [112, "example@google.com", true];
---
+++ 7_Tuples/tuples.ts
@@ -0,0 +1,12 @@
+//when we want specific thing in order in an array
+// const user: (string | number | boolean)[] = ["ks", 1, true];
+
+let User: [string, number, boolean];
+
+User = ["ks", 123, true]; // order is maintained
+
+let rgb :[number, number, number] = [255, 123, 112];
+
+let User2 : [number, string, boolean];
+
+User2 = [112, "example@google.com", true];
